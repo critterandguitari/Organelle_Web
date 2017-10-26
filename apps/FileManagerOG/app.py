860d21ba888cfa8603c973dc3e41ba94d21e6797
Owen Osborn <owen@critterandguitari.com>
2017-10-26 19:16:00 +0000

    unzip uploaded zips automatically

--- apps/FileManagerOG/app.py
+++ apps/FileManagerOG/app.py
@@ -67,6 +67,15 @@ class Root():
                 size += len(data)
                 newfile.write(data)
         print "saved file, size: " + str(size)
+        # check if it was a zip, unzip and delete orig if so
+        p, ext = os.path.splitext(filepath)
+        if ext == ".zip" :
+            print "that was a zip, gonna unzip"
+            zip_path = filepath
+            zip_parent_folder =os.path.dirname(zip_path)
+            os.system("unzip -o \""+zip_path+"\" -d \""+zip_parent_folder+"\" -x '__MACOSX/*'")
+            os.remove(zip_path)
+        
         cherrypy.response.headers['Content-Type'] = "application/json"
         return '{"files":[{"name":"x","size":'+str(size)+',"url":"na","thumbnailUrl":"na","deleteUrl":"na","deleteType":"DELETE"}]}'
         
